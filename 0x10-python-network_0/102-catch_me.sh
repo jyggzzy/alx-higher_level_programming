8dd45f5dbdaf43502f3ced63c1c7d955aef4b945
jyggzzy <106764369+jyggzzy@users.noreply.github.com>
2023-01-16 23:58:53 +0100

    origin

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that makes a request to 
-
-# 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-
-curl "0.0.0.0:5000/catch_me" -sLX PUT -H "Origin: school" -d "user_id=98"
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "You got me!"

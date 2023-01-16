428f1a60652f999668e30fa2b4d9270e38a3d717
jyggzzy <106764369+jyggzzy@users.noreply.github.com>
2023-01-16 23:59:46 +0100

    origin

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that sends a DELETE request to the URL 
-
-# passed as the first argument and displays the body of the response
-
-curl -sX DELETE "$1"
+# Script that sends a DELETE request to the URL passed
+curl -s "$1" -X DELETE

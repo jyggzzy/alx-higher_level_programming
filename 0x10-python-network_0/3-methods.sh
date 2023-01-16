604e61ba13c8a6f01977085fc3d3294cf09a1494
jyggzzy <106764369+jyggzzy@users.noreply.github.com>
2023-01-17 00:00:23 +0100

    origin

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# script that takes in a URL and 
-
-# displays all HTTP methods the server will accept.
-
-curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
+# Script that takes an URL and shows the Allowed OPTIONS
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-

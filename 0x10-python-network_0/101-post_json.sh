e7faff71b9ec3dab5ee9ce4d48e571af2b6138ba
jyggzzy <106764369+jyggzzy@users.noreply.github.com>
2023-01-16 23:57:19 +0100

    origin

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that sends a request 
-
-# to a URL passed as an argument, and displays only the status code of the response.
-
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"

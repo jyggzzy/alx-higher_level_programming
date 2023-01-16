9cdaa3b5e903c490ca21b2288a0b7db4f63efee5
jyggzzy <106764369+jyggzzy@users.noreply.github.com>
2023-01-16 23:54:51 +0100

    origin

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that sends a request 
-
-# to a URL passed as an argument, and displays only the status code of the response.
-
-curl -so /dev/null --write-out "%{http_code}" "$1"
+# Script that shows the response body redirecting /route_1
+curl -Ls "$1"

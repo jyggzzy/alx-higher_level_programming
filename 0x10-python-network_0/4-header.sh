0d549525e970a7f0a27aabce88147c7e3255dfac
jyggzzy <106764369+jyggzzy@users.noreply.github.com>
2023-01-17 00:01:59 +0100

    origin

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that takes in a URL as an argument, 
-
-# sends a GET request to the URL, and displays the body of the response
-
-curl -sX GET -H "X-School-User-Id: 98" "$1"
+# Script that takes an URL and displays the body of the response
+curl -s "$1" -H "X-School-User-Id"
